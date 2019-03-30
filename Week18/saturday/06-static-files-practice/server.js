940363cff7e3f37fb369ba4dd50a73d47423d29c
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-03-30 13:53:01 -0700

    added full stack pet project

--- Week18/saturday/06-static-files-practice/server.js
+++ Week18/saturday/06-static-files-practice/server.js
@@ -7,10 +7,14 @@ var con = mysql.createConnection({
 	host: "localhost",
 	user: "root",
 	password: "password",
-	database: "sales_db"
+	database: "tvshows_db"
 });
 
 app.use(express.static("public"));
+con.connect(function (err, result) {
+	if (err) throw err;
+	console.log("Connected!");
+});
 
 
 //you won't see this because it'll serve index.html because of app.use(express.static("public"));
@@ -18,6 +22,12 @@ app.get('/', function (req, res) {
 	res.send('blue sky');
 });
 
+app.get('/tvshows', function (req, res) {
+	con.query('SELECT * FROM tvshows', function (error, results, fields) {
+		if (error) res.send(error)
+		else res.json(results);
+	});
+});
 
 
 app.listen(3001, function () {
