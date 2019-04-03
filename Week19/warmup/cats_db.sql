279e8ddce93c3ca2386bd1013bbf53fb8c267866
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-02 18:42:27 -0700

    created npm package for warmup

--- Week19/warmup/cats_db.sql
+++ Week19/warmup/cats_db.sql
@@ -1,6 +1,18 @@
-// make a database called cats_db
 
-// make a table called cats
-//     the table should have an id and a place_name column
 
-// insert 4 records into the cats table
+--make a database called cats_db
+CREATE DATABASE cats_db;
+
+USE cats_db;
+
+--make a table called cats
+CREATE TABLE cats (
+    id INT AUTO_INCREMENT,
+    place_name VARCHAR(255),
+    PRIMARY KEY(id)
+);
+--    the table should have an id and a place_name column
+
+INSERT INTO cats (place_name) VALUES ('kitty'),('kittycat'),('meow'),('purr');
+
+-- insert 4 records into the cats table
