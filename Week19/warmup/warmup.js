b900fda2117030a633d78677828a59bd33a9be02
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-04 18:19:24 -0700

    added notes

--- Week19/warmup/warmup.js
+++ Week19/warmup/warmup.js
@@ -11,10 +11,20 @@ var connection = mysql.createConnection({
 connection.connect();
 app.use(express.static("public"));
 
+
 app.get('/cat.json', function (req, res) {
     connection.query('SELECT * FROM cats', function (error, results, fields) {
         if (error) throw error;
         res.json(results);
+    });
+})
+
+app.get('/cats-insert', function (req, res) {
+    connection.query('INSERT INTO cats (place_name) VALUES (?)', [req.body.place_name], function (error, results, fields) {
+        if (error) throw error;
+        res.json({
+            message: 'Success'
+        });
     });
 })
 
