9b3787d0c52badfd23513d2ecf97e5a5f5fc0671
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-07 18:54:22 -0700

    first example on using a constructor

--- Week22/warmup.js
+++ Week22/warmup.js
@@ -1,10 +1,14 @@
-class Person(name, height){
-    constructor(name, height){
+class Person {
+    constructor(name, height) {
         this.name = name;
         this.height = height;
     }
 
-    greeting(){
+    greeting() {
         console.log(`Hi my name is ${this.name}`)
     }
 }
+
+var p = new Person('khoa', 5.6);
+console.log(p.height);
+p.greeting();
